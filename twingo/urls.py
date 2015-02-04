72fd7d72d522add8ba20a07d8a627a598ead43ea
Jun-ya HASEBA <7pairs@gmail.com>
2015-02-04 19:22:03 +0900

    Fix the bug #9

--- twingo/urls.py
+++ twingo/urls.py
@@ -3,8 +3,13 @@
 from django.conf.urls import patterns, url
 
 
-urlpatterns = patterns('twingo_oauth',
-    url(r'^login/$', 'twingo.views.twitter_login', name='twingo_login'),
-    url(r'^callback/$', 'twingo.views.twitter_callback', name='twingo_callback'),
-    url(r'^logout/$', 'twingo.views.twitter_logout', name='twingo_logout'),
+urlpatterns = patterns('twingo.views',
+    # ログイン
+    url(r'^login/$', 'twitter_login', name='twingo_login'),
+
+    # Twitterからのコールバック
+    url(r'^callback/$', 'twitter_callback', name='twingo_callback'),
+
+    # ログアウト
+    url(r'^logout/$', 'twitter_logout', name='twingo_logout'),
 )
